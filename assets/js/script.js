c96794d861892a9647df06b5aec6df5fd13f524e
Felipe Zintzun <felipemendoza@felipes-mbp.lan>
2021-12-05 21:10:58 -0600

    added a readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,136 +18,95 @@ var questionDiv = document.querySelector(".questions-container");
 let formDiv = document.querySelector(".form-container");
 var leaderboard = document.querySelector(".user-scores");
 
-
-
-
 var qTitle = document.getElementById("question-title");
 var qChoices = document.getElementById("question-choices");
 
 // NEW GAME FUNCTION
 function newGame() {
-    // Set gameEnd variable to FALSE and start game
     gameStop = false;
-    // Reset score
     gameScore = 0;
-
-    // Initalize question set 
     currentQuestion = 0;
-
-    // ** REMOVE ??? ** //
-    // Reset answer array for new game
     userAnswers = [];
-
-    // ** Initialize Timer ** //
-    // Define amount of game time (in seconds)
     count = 75;
-    // call timer function to initiate timerInterval
+  
     timer();
-    // Show time on the DOM
+    
     time.textContent = count;
 
-    // Hide 'welcome-container' div
+
     welcomeDiv.classList.add("hide");
-    // Un-hide 'question-container' div
     questionDiv.classList.remove("hide");
 
-    // Run check function
     check();
 };
 
 // TIMER FUNCTION
 function timer() {
-    // Create a new timer
    timerInterval = setInterval(function() {
-       // decrement timer count
        count--;
-       // update display in DOM
        time.textContent = count;
 
-       // Test - Time rus out 
        if (count === 0) {
-           // Run gameOver function
            gameOver();
        }
-   }, 1000)  // Run every 1000 ms (or 1 second)
+   }, 1000) 
 };
 
 // CHECKS NUMBER OF QUESTIONS
 function check() {
-    // TEST HOW MANY QUESTIONS LEFT
     if (currentQuestion >= questions.length) {
-        // Run gameOver function
         gameOver();
     } else {
         loadQuestion();
     }
 };
 
-
-
 // LOAD QUESTIONS
 function loadQuestion() {
-    // Clear question title
     qTitle.textContent = '';
     qChoices.textContent = '';
 
     for (var i = 0; i < questions[currentQuestion].choices.length; i++) {
         qTitle.textContent = questions[currentQuestion].title;
 
-        //-- Render a new <li> for each question choice --//
-        // Create li element for each answer choice
         var ansChoice = document.createElement("li");
-        // Add 'id' attribute to each choice 
         ansChoice.setAttribute("id", i);
-        // Add 'data' attribute to each choice
         ansChoice.setAttribute("data-name", `data-choice-${i}`);
         ansChoice.setAttribute("value", questions[currentQuestion].choices[i]);
-        // Add our class containing the CSS styling 
         ansChoice.classList.add("ans-choice");
 
 
-        // Add event listener
         ansChoice.addEventListener("click", next)
-        // Update text of li element
         ansChoice.textContent = questions[currentQuestion].choices[i];
 
-        // Add answer choice to <ul> DOM
         qChoices.appendChild(ansChoice);
     }
 
 };
 
-
-
+// GOES TO NEXT QUESTION
 function next(e){
     if(event.target.innerText === questions[currentQuestion].answer) {
         gameScore += 10;
     }
 
-    // Increment currentQuestion
     currentQuestion++;
 
-    // Run check
     check();
 
-}
+};
+
 // GAME OVER 
 function gameOver() {
-    // Set gameStop variable to TRUE
     gameStop = true;
 
-    // Clear countdown timer
     clearInterval(timerInterval);
     time.textContent = "- -";
 
-    // Hide question container
     questionDiv.classList.add("hide");
 
-    // Display Game Score
     score.textContent = gameScore;
-    // Un-hide form container
     formDiv.classList.remove("hide");
-    // Clear form input field
     username.value = '';
 };
 
